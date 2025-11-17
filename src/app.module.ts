e09c731813556f75f6d75fe521ae758d5b882553
Mohamed Affes <med123011@gmail.com>
2025-11-17 18:38:36 +0100

    feat(database): integrate TypeORM with PostgreSQL and add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [],
+      synchronize: true,
+    }), // Database configuration
+  ],
   providers: [AppService],
+  controllers: [AppController], // Import the AppController
 })
 export class AppModule {}
